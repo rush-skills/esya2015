8d20112a1266042df77df19fd4f47b3f26dd9af8
Prelang Builder <builder@prelang.com>
2015-06-18 13:21:09 +0000

    Edited the 'devise_for' route options to skip sessions/registrations since we're going to override them

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions", passwords: "users/passwords"}
+  devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions", passwords: "users/passwords"}, skip: [:sessions, :registrations]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
