97482767dfa4352a501e6ebd3c1cc2b7891ceb29
Prelang Builder <builder@prelang.com>
2015-06-18 13:21:09 +0000

    Created editable Devise controllers

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,15 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  include ApplicationHelper
+
+  def create
+    super
+  end
+
+  def new
+    super
+  end
+
+  def edit
+    super
+  end
+end
