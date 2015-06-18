cae99886cc220432b2aaaeba7e0adea4ea11c149
Prelang Builder <builder@prelang.com>
2015-06-18 13:21:51 +0000

    Generated scaffolding for model 'Team' via Rails

---
+++ app/views/teams/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @team, :id, :tid, :team_name, :event_id, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :teams
+
   resources :participant_teams
 
   resources :registrations
