f9444b247138592e3b1afd25c52f73aaceebd37a
Prelang Builder <builder@prelang.com>
2015-06-18 13:21:29 +0000

    Generated model Event with migration

---
+++ db/migrate/20150618132129_create_events.rb
@@ -0,0 +1,20 @@
+class CreateEvents < ActiveRecord::Migration
+  def change
+    create_table :events do |t|
+      t.string :name
+      t.string :type
+      t.text :rules
+      t.text :judging
+      t.datetime :event_date_time
+      t.integer :team_size
+      t.string :venue
+      t.string :eligibilty
+      t.datetime :registration_deadline
+      t.text :prizes
+      t.text :contact
+      t.string :photo
+
+      t.timestamps
+    end
+  end
+end
