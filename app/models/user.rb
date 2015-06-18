778e98f931e025f392828b24ea37863cdf5e6205
Prelang Builder <builder@prelang.com>
2015-06-18 13:21:41 +0000

    Installed relation 'belongs_to' on model 'User'

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :trackable, :validatable
   has_many :event_admins
+  belongs_to :event
 end
