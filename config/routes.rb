8da6cf0417542b201860eb9606f4ba1f568172ee
Prelang Builder <builder@prelang.com>
2015-06-18 13:21:10 +0000

    Overrode Devise routes for new stylized paths

--- config/routes.rb
+++ config/routes.rb
@@ -54,4 +54,17 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  
+  #->Prelang (user_login:devise/stylized_paths)
+  devise_scope :user do
+    get    "login"   => "users/sessions#new",         as: :new_user_session
+    post   "login"   => "users/sessions#create",      as: :user_session
+    delete "signout" => "users/sessions#destroy",     as: :destroy_user_session
+    
+    get    "signup"  => "users/registrations#new",    as: :new_user_registration
+    post   "signup"  => "users/registrations#create", as: :user_registration
+    put    "signup"  => "users/registrations#update", as: :update_user_registration
+    get    "account" => "users/registrations#edit",   as: :edit_user_registration
+  end
+
 end
