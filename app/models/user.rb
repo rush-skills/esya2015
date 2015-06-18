e4b7b95173ebe9cd2965176a98a4c430976a5044
Prelang Builder <builder@prelang.com>
2015-06-18 13:21:40 +0000

    Installed relation 'has_many' on model 'User'

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+        :recoverable, :rememberable, :trackable, :validatable
+  has_many :event_admins
 end
