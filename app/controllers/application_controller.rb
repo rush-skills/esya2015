335bd55529dba7c269f210d697ed4921a01f288b
Ankur Singh <ankur13019@iiitd.ac.in>
2015-06-24 00:59:38 +0530

    WIP authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to main_app.root_path, :alert => exception.message
+  end
+
   private
-  
+
   #-> Prelang (user_login:devise)
   def require_user_signed_in
     unless user_signed_in?
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -29,7 +29,7 @@ class Ability
     # See the wiki for details:
     # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
 
-    can :read, :all                   # allow everyone to read everything
+    # can :read, :all                   # allow everyone to read everything
     if user# && user.admin?
       can :access, :rails_admin       # only allow admin users to access Rails Admin
       can :dashboard                  # allow access to dashboard
@@ -40,6 +40,20 @@ class Ability
       # elsif user.role? :sales
       #   can :update, Product, :hidden => false  # allow sales to only update visible products
       # end
+      if user.role.super_admin?
+        can :manage, :all             # allow superadmins to do anything
+        can :history, :all
+      elsif user.role.admin?
+        can :manage, :all
+        can :history, :all
+        cannot :delete, [Participant,ParticipantTeam,Registration,Team]
+      elsif user.role.event_head?
+        # can :manage, Event, object if object.user
+      else
+        # can :manage, [Blogger, Journalist, FailedJournalistAttempt, FailedBloggerAttempt, PublishedBlogger, PublishedJournalist]  # allow users to do anything to journalist and blogger related stuff
+        # cannot :destroy, [Blogger, Journalist, FailedJournalistAttempt, FailedBloggerAttempt, PublishedBlogger, PublishedJournalist]  # dont allow users to delete journalist and blogger related stuff
+        # can :read, [Beat,Project,User]
+      end
     end
   end
 end
