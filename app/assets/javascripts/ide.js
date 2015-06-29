62e327e47f76769ed52d248d773a7d007bf58def
Ankur Singh <ankur13019@iiitd.ac.in>
2015-06-29 16:50:05 +0530

    formatted code

--- app/assets/javascripts/ide.js
+++ app/assets/javascripts/ide.js
@@ -21,4 +21,37 @@
 //= require ide/events
 //= require ide/pageContent
 
-$(function(){ $(document).foundation(); });
+$(function(){
+  $(document).foundation();
+
+ editor = new wysihtml5.Editor("wysihtml5-editor", {
+    toolbar:     "wysihtml5-editor-toolbar",
+    stylesheets: ["/css/reset.css", "/css/editor.css"],
+    parserRules: wysihtml5ParserRules
+  });
+
+  editor.on("load", function() {
+    var composer = editor.composer;
+    composer.selection.selectNode(editor.composer.element.querySelector("h1"));
+  });
+
+  jQuery(function($, undefined) {
+    $('#commandLine').terminal(function(command, term) {
+      if (command !== '') {
+        try {
+          var result = window.eval(command);
+          if (result !== undefined) {
+            term.echo(new String(result));
+          }
+        } catch(e) {
+          term.error(new String(e));
+        }
+      } else {
+       term.echo('');
+     }
+   }, {
+    greetings: 'Welcome to ESYA\nType ? for help',
+    name: 'js_demo',
+    prompt: 'you@esya.iiitd.ac.in> '});
+  });
+});
