4bac6e1956db3937ae3a284f135e2227adbc5023
Ankur Singh <ankur13019@iiitd.ac.in>
2015-06-30 22:49:25 +0530

    added first level ls command

--- app/assets/javascripts/ide/main.js
+++ app/assets/javascripts/ide/main.js
@@ -14,7 +14,17 @@ $(function(){
 
   jQuery(function($, undefined) {
     $('#commandLine').terminal(function(command, term) {
-      if (command !== '') {
+      load();
+      if(command == 'ls'){
+        if(current_directory==root_directory){
+          // term.echo([pages.join(' ')],{color: "lightskyblue"});
+          term.echo("[[;red;black]"+pages.join('] [[;red;black]')+ "] [[;blue;black]"+categories.join('] [[;blue;black]')+"]" );
+
+          // [[guib;<COLOR>;<BACKGROUND>]some text]
+          // term.echo([categories.join(' ')],{color: "red"})
+        }
+      }
+      else if (command !== '') {
         try {
           var result = window.eval(command);
           if (result !== undefined) {
@@ -32,3 +42,62 @@ $(function(){
     prompt: 'you@esya.iiitd.ac.in> '});
   });
 });
+
+
+function load(){
+  root_directory = "/esya15/";
+  current_directory = "/esya15/";
+  categories = ["CSE","ECE","Flagship","Non_tech","School","Workshop"];
+  events = [];
+  $('.event_link').each(function(){
+    events.push($(this).attr('data-code'));
+  });
+  pages = [];
+  $('.nav_entry').each(function(){
+    pages.push($(this).attr('data-code'));
+  });
+  function eventFilter(category){
+    cat_events = [];
+    $('.event_link[data-category='+category+']').each(function(){
+      cat_events.push($(this).attr('data-code'));
+    });
+    return cat_events;
+  }
+}
+
+
+
+function load2(){
+  function load(){
+  root_directory = "/esya15/";
+  current_directory = "/esya15/";
+  categories = ["CSE","ECE","Flagship","Non_tech","School","Workshop"];
+  events = [];
+  $('.event_link').each(function(){
+    events.push($(this).find('a').html());
+  });
+  event_ids = new Object();
+  $('.event_link').each(function(){
+    event_ids[$(this).find('a').html()]=$(this).attr('data-code');
+  });
+  pages = [];
+  $('.nav_entry').each(function(){
+    pages.push($(this).find('a').html());
+  });
+  page_ids = new Object();
+  $('.nav_entry').each(function(){
+    page_ids[$(this).find('a').html()]=$(this).attr('data-code');
+  });
+  function eventFilter(category){
+    cat_events = [];
+    $('.event_link[data-category='+category+']').each(function(){
+      cat_events.push($(this).find('a').html());
+    });
+    return cat_events;
+  }
+  function getEventCode(event)
+  {
+    return event_ids[event];
+  }
+}
+}
